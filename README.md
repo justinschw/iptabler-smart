0339ee8a9a68239e3ad43e07ab1685447871c664
Justin Schwartzbeck <justinmschw@gmail.com>
2021-08-31 09:47:33 -0500

    Adds code

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # iptabler-smart
-A wrapper for the iptabler module with some useful functionality
+A wrapper for the iptabler module with some useful functionality.
+
+The main thing this module offers is the ability to only apply rules that aren't already applied. Currently iptabler will raise a signal and cause your program to terminate if you apply a rule that already exists. This module's applyRuleSafely() method will check against existing firewall rules and only create the rule if it doesn't already exist.
+
+## Usage
+```
+const fw = new iptabler();
+
+const createMyChain = {
+    sudo: true,
+    table: 'nat',
+    new_chain: 'MYCHAIN'
+};
+
+
+fw.applyRuleSafely(createMyChain).then(() => {
+
+    // Apply the rule safely, fetching existing rules first
+    return fw.applyRuleSafely(createMyChain);
+
+}).then(() => {
+
+    // Applying the same rule again. This time it won't be applied since it is already present.
+    return fw.applyRuleSafely(createMyChain);
+
+}).then(() => {
+
+    // Apply the rule safely, without first fetching existing rules
+    return fw.applyRuleSafely({
+        sudo: true,
+        table: 'nat',
+        new_chain: 'MYCHAIN2'
+    }, false);
+
+}).then(() => {
+
+    let newChainRules = [
+        {
+            sudo: true,
+            table: 'nat',
+            append: 'POSTROUTING',
+            jump: 'MYCHAIN'
+        },
+        {
+            sudo: true,
+            table: 'nat',
+            append: 'MYCHAIN',
+            out_interface: 'cni0',
+            jump: 'MASQUERADE'
+        }
+    ];
+
+    /*
+     * Apply the array of rules. Fetches existing rules at the beginning,
+     * and then applies the rules after.
+     */
+    return fw.applyRulesSafely(newChainRules);
+
+}).then(() => {
+
+    // Fetch all the rules manually so they will be up-to-date for next time
+    return fw.getAllRules();
+
+}).then(() => {
+    console.log('done');
+});
+```
+
+## Methods
+|Method name|Parameters|Function|
+|-----------|----------|--------|
+|getAllRules|None|Fetches existing rules so we know what has been applied|
+|applyRuleSafely|<ul><li>**_rule_** - iptabler rule </li><li> **_updateCurrent_** - call getAllRules before running this rule (default: true)</li></ul>|Apply rule only if it hasn't been applied already|
+|applyRulesSafely|<ul><li>**_rules_** - array of iptabler rules</li></ul>|Calls applyRuleSafely for each rule in the array, only calling getAllRules at the beginning|
+|applyRule|<ul><li>**_rule_** - iptabler rule</li></ul>|Simply execs iptabler rule without checking (unsafe)|
+|applyRules|<ul><li>**_rules_** - array of iptabler rules</li></ul>|calls applyRule on all rules in the array (unsafe)|
---
+++ test/iptablerSmart.js
@@ -0,0 +1,202 @@
+'use strict';
+const mockRequire = require('mock-require');
+const realIptabler = require('iptabler');
+let errors = [];
+let outputs = [];
+/*
+ * Create mock iptabler object
+ */
+
+mockRequire('iptabler', function(options) {
+    const real = realIptabler(options);
+    return {
+        _args: real._args,
+        exec: function(fn) {
+            const err = errors.shift();
+            if (err) {
+                return fn(err, null);
+            }
+            return fn(null, outputs.shift());
+        }
+    }
+})
+
+const IptablerSmart = require('../lib/iptablerSmart');
+const iptabler = require('iptabler');
+const sandbox = require('sinon').createSandbox();
+const expect = require('chai').expect;
+
+const getAllRulesResult = `
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-N ROUTER-FORWARD
+-A FORWARD -j ROUTER-FORWARD
+-A ROUTER-FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A ROUTER-FORWARD -i eth1 -o eth0 -j ACCEPT
+-A ROUTER-FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A ROUTER-FORWARD -i wlan0 -o eth0 -j ACCEPT
+`;
+
+const getAllRulesNatResult = `
+-P PREROUTING ACCEPT
+-P INPUT ACCEPT
+-P POSTROUTING ACCEPT
+-P OUTPUT ACCEPT
+-N ROUTER-INGRESS
+-N ROUTER-EGRESS
+-A PREROUTING -j ROUTER-INGRESS
+-A POSTROUTING -j ROUTER-EGRESS
+-A ROUTER-INGRESS -s 192.168.5.1/32 -j RETURN
+-A ROUTER-INGRESS -s 192.168.6.1/32 -j RETURN
+-A ROUTER-EGRESS -o eth0 -j MASQUERADE
+`;
+
+describe('/lib/iptablerSmart', function() {
+    describe('getAllRules', function() {
+        beforeEach(function() {
+            errors = []
+            outputs = [];
+        });
+
+        it('valid', function(done) {
+            outputs.push(getAllRulesResult);
+            outputs.push(getAllRulesNatResult);
+            const fw = new IptablerSmart();
+            fw.getAllRules().then(() => {
+                expect(JSON.stringify(fw.allRules)).eql(JSON.stringify(getAllRulesResult.split('\n')));
+                expect(JSON.stringify(fw.allNatRules)).eql(JSON.stringify(getAllRulesNatResult.split('\n')));
+                done();
+            });
+        });
+
+        it('failed', function(done) {
+            errors.push(new Error('iptables failure'));
+            const fw = new IptablerSmart();
+            fw.getAllRules().then(() => {
+                done(new Error('should have thrown error'));
+            }).catch(err => {
+                done();
+            });
+        });
+    }); // end getAllRules
+
+    describe('applyRuleSafely', function() {
+        beforeEach(function() {
+            outputs.push(getAllRulesResult);
+            outputs.push(getAllRulesNatResult);
+        });
+
+        it('new rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRuleSafely({
+                sudo: true,
+                table: 'nat',
+                append: 'PREROUTING',
+                jump: 'MYCHAIN'
+            }).then(() => {
+                done();
+            })
+        });
+
+        it('existing rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRuleSafely({
+                sudo: true,
+                table: 'nat',
+                append: 'ROUTER-INGRESS',
+                source: '192.168.5.1/32',
+                jump: 'RETURN'
+            }).then(() => {
+                done();
+            })
+        });
+    }); // end applyRuleSafely
+
+    describe('applyRulesSafely', function() {
+        beforeEach(function() {
+            outputs.push(getAllRulesResult);
+            outputs.push(getAllRulesNatResult);
+        });
+
+        it('new rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRulesSafely([{
+                sudo: true,
+                table: 'nat',
+                append: 'PREROUTING',
+                jump: 'MYCHAIN'
+            }]).then(() => {
+                done();
+            })
+        });
+
+        it('existing rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRulesSafely([{
+                sudo: true,
+                table: 'nat',
+                append: 'ROUTER-INGRESS',
+                source: '192.168.5.1/32',
+                jump: 'RETURN'
+            }]).then(() => {
+                done();
+            })
+        });
+    }); // end applyRulesSafely
+
+    describe('applyRule', function() {
+        it('new rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRule({
+                sudo: true,
+                table: 'nat',
+                append: 'PREROUTING',
+                jump: 'MYCHAIN'
+            }).then(() => {
+                done();
+            })
+        });
+
+        it('existing rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRule({
+                sudo: true,
+                table: 'nat',
+                append: 'ROUTER-INGRESS',
+                source: '192.168.5.1/32',
+                jump: 'RETURN'
+            }).then(() => {
+                done();
+            })
+        });
+    }); // end applyRule
+
+    describe('applyRules', function() {
+
+        it('new rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRules([{
+                sudo: true,
+                table: 'nat',
+                append: 'PREROUTING',
+                jump: 'MYCHAIN'
+            }]).then(() => {
+                done();
+            })
+        });
+
+        it('existing rule', function(done) {
+            const fw = new IptablerSmart();
+            fw.applyRules([{
+                sudo: true,
+                table: 'nat',
+                append: 'ROUTER-INGRESS',
+                source: '192.168.5.1/32',
+                jump: 'RETURN'
+            }]).then(() => {
+                done();
+            })
+        });
+    }); // end applyRules
+});
